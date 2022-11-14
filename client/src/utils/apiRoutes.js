234e0ada2c6433ebbf0f044267ca0e37d54d51bf
Valentin <93974733+valentindush@users.noreply.github.com>
2022-11-14 09:19:42 +0200

    deploy

--- client/src/utils/apiRoutes.js
+++ client/src/utils/apiRoutes.js
@@ -1,3 +1,3 @@
-export const host = "http://localhost:4000";
+export const host = "https://dushforms.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/signup`;
