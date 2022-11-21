622037dfaeb948332667c9dbbb1e5f30e03d24a9
oreste-abizera <oresteabizera11@gmail.com>
2022-11-21 21:41:05 +0200

    fixed AUTH error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/login'
 import SignUp from './pages/signup'
 import Home from './pages/home'
@@ -10,17 +10,18 @@ import Create from './pages/create'
 import ViewForm from './pages/ViewForm'
 
 function App() {
-  
+  const [token, setToken] = useState(null)
+  useEffect(() => {
+    setToken(localStorage.getItem("forms_token"))
+  }, [])
 
   return (
-    
+
     <Router>
       <Routes>
-        <Route path='/login' element={<Login/>} />
-        <Route exact path='/signup' element={<SignUp/>}/>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/create' element={<Create/>}/>
-        <Route path='/form/:url' element={<ViewForm/>}/>
+        <Route path='/login' element={<Login />} />
+        <Route exact path='/signup' element={<SignUp />} />
+        {token && <><Route path='/' element={<Home />} /><Route path='/create' element={<Create />} /><Route path='/form/:url' element={<ViewForm />} /></>}
       </Routes>
     </Router>
   )
