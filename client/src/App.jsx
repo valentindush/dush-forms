46f448c0072b8a0c8e2d3ad9793c93643c780b9b
Valentin <93974733+valentindush@users.noreply.github.com>
2022-12-29 22:53:56 +0200

    fx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,36 @@
-import { useState , useEffect} from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './pages/login'
-import SignUp from './pages/signup'
-import Home from './pages/home'
-import Create from './pages/create'
-import ViewForm from './pages/ViewForm'
-import { useNavigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import SignUp from "./pages/signup";
+import Home from "./pages/home";
+import Create from "./pages/create";
+import ViewForm from "./pages/ViewForm";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-  const [token, setToken] = useState(null)
+  const [token, setToken] = useState(null);
   useEffect(() => {
-    setToken(localStorage.getItem("forms_token"))
-  }, [])
+    setToken(localStorage.getItem("forms_token"));
+  }, []);
 
   return (
-
     <Router>
       <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route exact path='/signup' element={<SignUp />} />
-        {token && <><Route path='/' element={<Home />} /><Route path='/create' element={<Create />} /><Route path='/form/:url' element={<ViewForm />} /></>}
+        <Route path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<SignUp />} />
+        {token && (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/form/:url" element={<ViewForm />} />
+          </>
+        )}
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
